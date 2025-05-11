621a573b10ba059e8a59cad2e1a9b7d903e4f9bd
jasonnucsd <122554116+jasonnucsd@users.noreply.github.com>
2025-05-11 13:33:08 -0700

    Create unit.test.js

---
+++ tests/unit.test.js
@@ -0,0 +1,10 @@
+test('adds 1 + 1 to equal 2', () => {
+  expect(1 + 1).toBe(2);
+});
+
+// Example of importing from another file:
+/*import { sum } from '../code-to-unit-test/sum';
+
+test('adds 1 + 2 to equal 3', () => {
+  expect(sum(1,2)).toBe(3);
+});*/
