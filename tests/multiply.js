1b444602cf8325288e32e69ea872cd67c712065c
kecohen575 <kevin@cohen26.com>
2025-05-20 22:28:33 -0700

    test didnt work. reverted.

--- tests/multiply.js
+++ tests/multiply.js
@@ -2,8 +2,8 @@
  * Multiples a and b.
  * 
  * @function multiply
- * @param {number} a - First Number.
- * @param {number} b - Second Number.
+ * @param {number} a - 1st Number.
+ * @param {number} b - 2nd Number.
  * @returns {number} The product of `a` and `b`.
  */
 function multiply(a, b) {
