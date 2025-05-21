9e225a62936f79d3c285b2e122e26a28ee67d0c0
kecohen575 <kevin@cohen26.com>
2025-05-20 22:01:07 -0700

    added scripts to auto populate function documentation into .github/docs

--- tests/sum.js
+++ tests/sum.js
@@ -1,3 +1,11 @@
+/**
+ * Adds two numbers together.
+ *
+ * @function sum
+ * @param {number} a - The first addend.
+ * @param {number} b - The second addend.
+ * @returns {number} The result of adding `a` and `b`.
+ */
 function sum(a, b) {
     return a + b;
 }
