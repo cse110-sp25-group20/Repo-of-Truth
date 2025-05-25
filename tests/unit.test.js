069de1da46a1683f97e9585afb5d1049755a0797
Juan Pablo Davalos <davjuanpablo423@gmail.com>
2025-05-25 12:17:06 -0700

    removed multiply and sum files again

--- tests/unit.test.js
+++ tests/unit.test.js
@@ -1,21 +1,5 @@
-const sum = require('../tests/sum');
-const multiply = require('../tests/multiply');
-
-test('adds 1 + 1 to equal 2', () => {
-  expect(1 + 1).toBe(2);
-});
-
-// Example of importing from another file:
-/*import { sum } from '../code-to-unit-test/sum';
-
-test('adds 1 + 2 to equal 3', () => {
-  expect(sum(1,2)).toBe(3);
-});*/
-
-test('adds 1 + 2 to equal 3', () => {
-  expect(sum(1, 2)).toBe(3);
-});
-
-test('multiplies 3 * 2 to equal 6', () => {
-  expect(multiply(3, 2)).toBe(6);
+describe("Unit Tests", () => {
+  test("placeholder test", () => {
+    expect(true).toBe(true);
+  });
 });
