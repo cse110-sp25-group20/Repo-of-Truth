448705568bd434ec99412e80efd93085cf1257d3
Juan Pablo Davalos <davjuanpablo423@gmail.com>
2025-05-25 11:22:37 -0700

    added function descriptions for autodocs

--- source/binder/binder.js
+++ source/binder/binder.js
@@ -1,32 +1,37 @@
-import './pokemon-binder.js';
-
-function handleAddCard() {
-    const binder = document.querySelector('pokemon-binder');
-    binder.setPages([['card_back.png','','','','','','','','']]);
-}
-
-function turnPageRight() {
-    const binder = document.querySelector('pokemon-binder');
-    binder.flipForward();
-}
-
-function turnPageLeft() {
-    const binder = document.querySelector('pokemon-binder');
-    binder.flipBackward();
-}
-
-const addButton = document.getElementById('addCard');
-addButton.addEventListener('click', handleAddCard);
-
-const turnRightButton = document.getElementById('turnPageRight');
-turnRightButton.addEventListener('click', turnPageRight);
-
-const turnLeftButton = document.getElementById('turnPageLeft');
-turnLeftButton.addEventListener('click', turnPageLeft);
-
+import "./pokemon-binder.js";
 
 /**
- * @description A test function to verify JSDoc generation
- * @param {string} testParam - A test parameter
+ * @description Adds a new page to the binder with empty card slots and one card back image
  * @returns {void}
  */
+function handleAddCard() {
+  const binder = document.querySelector("pokemon-binder");
+  binder.setPages([["card_back.png", "", "", "", "", "", "", "", ""]]);
+}
+
+/**
+ * @description Flips the binder page forward to show the next two pages
+ * @returns {void}
+ */
+function turnPageRight() {
+  const binder = document.querySelector("pokemon-binder");
+  binder.flipForward();
+}
+
+/**
+ * @description Flips the binder page backward to show the previous two pages
+ * @returns {void}
+ */
+function turnPageLeft() {
+  const binder = document.querySelector("pokemon-binder");
+  binder.flipBackward();
+}
+
+const addButton = document.getElementById("addCard");
+addButton.addEventListener("click", handleAddCard);
+
+const turnRightButton = document.getElementById("turnPageRight");
+turnRightButton.addEventListener("click", turnPageRight);
+
+const turnLeftButton = document.getElementById("turnPageLeft");
+turnLeftButton.addEventListener("click", turnPageLeft);
