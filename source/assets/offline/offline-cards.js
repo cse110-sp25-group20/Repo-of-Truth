c5a9a67189622cf97c4e930f5947e06fc56eb6ea
panic360 <jinaidu@ucsd.edu>
2025-06-07 00:47:13 -0700

    Determine modal based on API fetching

---
+++ source/assets/offline/offline-cards.js
@@ -0,0 +1,6 @@
+// /source/assets/offline/offline-cards.js
+
+export const offlineCards = [
+  { name: "Pikachu",   imgPath: "/source/assets/offline/images/pikachu.png"   },
+  { name: "Charizard", imgPath: "/source/assets/offline/images/charmander.png" }
+];
