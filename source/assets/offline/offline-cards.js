9b27e6b5ee5f9eceac008bd18efcb2b290e689b3
panic360 <jinaidu@ucsd.edu>
2025-06-07 00:47:13 -0700

    Determine modal based on API fetching
    ioasndf

--- source/assets/offline/offline-cards.js
+++ source/assets/offline/offline-cards.js
@@ -1,30 +1,6 @@
 // /source/assets/offline/offline-cards.js
-// adding from here https://www.pastemagazine.com/games/pokemon/the-top-100-pokemon-of-all-time
 
 export const offlineCards = [
-  {
-    name: "Mewtwo",
-    imgPath: "/source/assets/offline/images/mewtwo.png",
-    imgPathBig: "/source/assets/offline/images/mewtwo-big.png"
-  },
-  {
-    name: "Pikachu",
-    imgPath: "/source/assets/offline/images/pikachu.png",
-    imgPathBig: "/source/assets/offline/images/pikachu-big.png"
-  },
-  {
-    name: "Suicune",
-    imgPath: "/source/assets/offline/images/suicune.png",
-    imgPathBig: "/source/assets/offline/images/suicune-big.png"
-  },
-  {
-    name: "Charizard",
-    imgPath: "/source/assets/offline/images/charizard.png",
-    imgPathBig: "/source/assets/offline/images/charizard-big.png"
-  },
-  {
-    name: "Rayquaza",
-    imgPath: "/source/assets/offline/images/rayquaza.png",
-    imgPathBig: "/source/assets/offline/images/rayquaza-big.png"
-  }
+  { name: "Pikachu",   imgPath: "/source/assets/offline/images/pikachu.png"   },
+  { name: "Charizard", imgPath: "/source/assets/offline/images/charmander.png" }
 ];
