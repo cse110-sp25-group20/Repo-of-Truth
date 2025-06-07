517482b8d5a04f04f03177a33c6b001c62a3ab65
recal <jmpnguyen04@gmail.com>
2025-06-07 02:58:33 -0700

    fixed offline modal info and added more fallback cards

--- source/assets/offline/offline-cards.js
+++ source/assets/offline/offline-cards.js
@@ -1,6 +1,51 @@
 // /source/assets/offline/offline-cards.js
 
+//todo: probably sort these by alphabetical order or some other order
+
 export const offlineCards = [
-  { name: "Pikachu",   imgPath: "/source/assets/offline/images/pikachu.png"   },
-  { name: "Charizard", imgPath: "/source/assets/offline/images/charmander.png" }
+{ name: 'Pikachu',
+  hp: 60,
+  types: ['Electric'],
+  rarity: '',
+  set: {
+    name: 'Pokemon Rumble'
+  },
+  imgUrl: 'source/assets/offline/images/pikachu.png'},
+
+{ name: 'Umbreon',
+  hp: 80,
+  types: ['Darkness'],
+  rarity: 'Rare Holo',
+  set: {
+    name: 'Neo Discovery'
+  },
+  imgUrl: 'source/assets/offline/images/umbreon.png'},
+
+{ name: 'Lugia',
+  hp: 80,
+  types: ['Psychic'],
+  rarity: 'Rare',
+  set: {
+    name: 'Unseen Forces'
+  },
+  imgUrl: 'source/assets/offline/images/lugia.png'},
+
+{ name: 'Bulbasaur',
+  hp: 40,
+  types: ['Grass'],
+  rarity: '',
+  set: {
+    name: 'Base'
+  },
+  imgUrl: 'source/assets/offline/images/bulbasaur.png'},
+
+{ name: 'Charizard',
+  hp: 160,
+  types: ['Fire'],
+  rarity: 'Rare Holo',
+  set: {
+    name: 'Boundaries Crossed'
+  },
+  imgUrl: 'source/assets/offline/images/charizard.png'},
 ];
+
